f1419277f6559e6328e9936a480c5c4ba62f4e0d
Monisha <monishaa0607@gmail.com>
2022-05-30 11:44:47 +0530

    initial commit

--- sbjpa12/src/main/java/com/cts/sbjpa/sbjpa12/service/ProductService.java
+++ sbjpa12/src/main/java/com/cts/sbjpa/sbjpa12/service/ProductService.java
@@ -6,28 +6,55 @@ import com.cts.sbjpa.sbjpa12.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * The type Product service.
+ */
 @Service
 public class ProductService {
 
     @Autowired
     private ProductRepository pRepository;
 
+    /**
+     * Save product product.
+     *
+     * @param product the product
+     * @return the product
+     */
     public Product saveProduct(Product product){
 
         return pRepository.save(product);
     }
 
+    /**
+     * Get product by id product.
+     *
+     * @param id the id
+     * @return the product
+     */
     public Product getProductById(int id){
         return pRepository.findById(id).orElse(null);
 
     }
 
+    /**
+     * Delete product by id string.
+     *
+     * @param id the id
+     * @return the string
+     */
     public String deleteProductById(int id){
         pRepository.deleteById(id);
         return "product deleted,{},id";
 
     }
 
+    /**
+     * Update product product.
+     *
+     * @param product the product
+     * @return the product
+     */
     public Product updateProduct(Product product){
         Product updateProduct=pRepository.getById(product.getId());
         updateProduct.setName(product.getName());
